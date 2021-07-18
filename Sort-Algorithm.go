df022d4abc76936a00b10786a28950eb8c834cc2
kennyZ <kennyZ96@hotmail.com>
2021-07-18 20:41:49 +0800

    [ADD] selectionSort

--- Sort-Algorithm.go
+++ Sort-Algorithm.go
@@ -22,13 +22,27 @@ func bubbleSort(nums []int) []int {
 	return nums
 }
 
-//
+//选择排序，时间复杂度为 O(n^2),空间复杂度为O(1),排序不稳定，原地排序。
+func selectionSort(nums []int) []int {
+	len := len(nums)
+	for i := 0; i < len-1; i++ {
+		min := i
+		for j := i; j < len; j++ {
+			if nums[j] <nums[min]{
+				min = j
+			}
+		}
+		nums[i], nums[min] = nums[min], nums[i]
+	}
+	return nums
+}
 
+//
 func main() {
 	nums1 := []int{19, 2, 22, 1, 25, 5, 34, 32, 65, 32, 7, 7, 2, 5756, 899, 6, 46}
-	bubbleSort(nums1)
+	selectionSort(nums1)
 	fmt.Println(nums1)
 	nums2 := []int{1, 2, 3, 4, 6, 6, 7, 9, 8}
-	bubbleSort(nums2)
+	selectionSort(nums2)
 	fmt.Println(nums2)
 }
