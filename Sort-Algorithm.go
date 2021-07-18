f09855c822076dd0e7abf72cdf107f54d3232321
kennyZ <kennyZ96@hotmail.com>
2021-07-18 20:30:53 +0800

    [ADD] bubble sorting

--- Sort-Algorithm.go
+++ Sort-Algorithm.go
@@ -1,1 +1,34 @@
-package leetcode_go
+//this package is used for kennyZ's review of sort algorithm
+package main
+
+import "fmt"
+
+//冒泡排序，时间复杂度为 O(n^2),空间复杂度为O(1),排序稳定，原地排序。
+//使用flag标识一次排序中是否发生过逆序，以此来略微提高效率
+func bubbleSort(nums []int) []int {
+	len := len(nums)
+	for i := 0; i < len-1; i++ {
+		flag := false
+		for j := 0; j < len-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+				flag = true
+			}
+		}
+		if flag == false {
+			return nums
+		}
+	}
+	return nums
+}
+
+//
+
+func main() {
+	nums1 := []int{19, 2, 22, 1, 25, 5, 34, 32, 65, 32, 7, 7, 2, 5756, 899, 6, 46}
+	bubbleSort(nums1)
+	fmt.Println(nums1)
+	nums2 := []int{1, 2, 3, 4, 6, 6, 7, 9, 8}
+	bubbleSort(nums2)
+	fmt.Println(nums2)
+}
